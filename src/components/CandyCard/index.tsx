782a1b7fa5bd7519a900868b30035b8c6d7add65
JungHyun <leahincom@gmail.com>
2021-07-04 00:04:54 +0900

    feat: 컴포넌트 div 수정 및 date 조건부 렌더링

--- src/components/CandyCard/index.tsx
+++ src/components/CandyCard/index.tsx
@@ -1,5 +1,4 @@
 import styled from 'styled-components';
-import Image from 'next/image';
 
 const Container = styled.div`
   display: flex;
@@ -8,61 +7,74 @@ const Container = styled.div`
   margin: 0;
   width: 254px;
   height: 383px;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans',
+    'Helvetica Neue', sans-serif;
 `;
 
 const Thumbnail = styled.img`
+  z-index: 2;
   border-top-left-radius: 17px;
   border-top-right-radius: 17px;
   width: 100%;
 `;
 
 const Metadata = styled.div`
+  box-sizing: border-box;
   display: flex;
   flex-direction: column;
   justify-content: center;
+  z-index: 1;
   border-bottom-left-radius: 17px;
   border-bottom-right-radius: 17px;
+  box-shadow: 5px 5px 20px #c4c4c471;
   background-color: white;
   padding: 23px 0 23px 17px;
   width: 100%;
 `;
 
 const Category = styled.h2`
+  margin: 0;
   margin-bottom: 4px;
   color: #4b4b4b;
   font-size: 16px;
+  font-weight: 400;
 `;
 
 const Name = styled.h1`
+  margin: 0;
   color: #252525;
   font-size: 24px;
+  font-weight: 700;
 `;
 
 const Date = styled.h3`
+  margin: 0;
   margin-top: 12px;
   color: #a0a0a0;
   font-size: 13px;
+  font-weight: 400;
 `;
 
 export interface CandyCardProps {
   image: string;
   category: string;
   name: string;
-  date?: Date;
+  createdDate: number;
+  plannedDate?: Date;
 }
 
-export default function TestCard({ image, category, name, date }: CandyCardProps) {
+export default function TestCard({ image, category, name, createdDate, plannedDate }: CandyCardProps) {
   return (
     <Container>
-      <Thumbnail>
-        <Image src={image} alt='me' layout='fill' className='candy__image' />
-      </Thumbnail>
+      <Thumbnail src={image} />
       <Metadata>
-        <div className='candy__metadata'>
-          <Category>{category}</Category>
-          <Name>{name}</Name>
-          <Date>{date}</Date>
-        </div>
+        <Category>{category}</Category>
+        <Name>{name}</Name>
+        <Date>
+          {plannedDate
+            ? `${plannedDate.getUTCMonth()}월 ${plannedDate.getUTCDate()}일 예정`
+            : `담은지 ${createdDate}일 되었어요.`}
+        </Date>
       </Metadata>
     </Container>
   );
