5fc021581acbccf8384c304c8cdb0c70262550a1
찬순 <ckstns1231@gmail.com>
2021-07-04 22:19:44 +0900

    feat : stories.tsx 수정

--- src/components/common/CategoryCard/index.stories.tsx
+++ src/components/common/CategoryCard/index.stories.tsx
@@ -1,18 +1,18 @@
 import { Meta, Story } from '@storybook/react';
 import CategoryCard, { CategoryCardProps } from '.';
 
-export default { title: 'components/common/RecommendCandyCard', component: CategoryCard } as Meta;
+export default { title: 'components/common/CategoryCard', component: CategoryCard } as Meta;
 
 const Template: Story<CategoryCardProps> = (props) => <CategoryCard {...props} />;
 
 export const Basic = Template.bind({});
 Basic.args = {
-    candyImg:'https://dummyimage.com/74.68x74.68/000/fff',
+    candyImg:'https://dummyimage.com/72x72/000/fff',
     category:'고생한 나를 위한 캔디',
     candynum:20,
     date:'3',
-    firstImg:'https://dummyimage.com/74.68x74.68/000/fff',
-    secondImg:'https://dummyimage.com/74.68x74.68/000/fff',
-    thirdImg:'https://dummyimage.com/74.68x74.68/000/fff',
+    firstImg:'https://dummyimage.com/184x255/000/fff',
+    secondImg:'https://dummyimage.com/151x127/000/fff',
+    thirdImg:'https://dummyimage.com/151x127/000/fff',
   
 };
