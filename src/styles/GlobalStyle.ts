fed2842045ad8538981429d85c91a7247eee5032
lebmouse <youngkwon@croquis.com>
2021-07-04 19:26:00 +0900

    refactor:  미사용 코드 제거

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -1,12 +1,10 @@
 import { createGlobalStyle } from 'styled-components';
 import colorSystems from './colorSystems';
-import HCMain from './fonts/HCMain/HCMain';
 import initializeStyle from './initializeStyle';
 
 const GlobalStyle = createGlobalStyle`
 ${initializeStyle};
 ${colorSystems};
-${HCMain};
 `;
 
 export default GlobalStyle;
