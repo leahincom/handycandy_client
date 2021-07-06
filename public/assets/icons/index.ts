ef635ec3b0a07a0ab56995b119888ec1bb410e7e
JungHyun <leahincom@gmail.com>
2021-07-06 17:25:43 +0900

    chore: icons 추가

--- public/assets/icons/index.ts
+++ public/assets/icons/index.ts
@@ -1,3 +1,5 @@
 export { default as CandyIconFlower } from './candyicon_flower.svg';
 export { default as Plus } from './plus.svg';
 export { default as CardMore } from './CardMore.svg';
+export { default as CategoryAdd } from './CategoryAdd.svg';
+export { default as ToggleButton } from './ToggleButton.svg';
