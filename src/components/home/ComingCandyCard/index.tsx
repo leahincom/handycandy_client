1dcce5a7d0f933e2b42b74dfd5572045bcf58d34
yoon <nicealice99@gmail.com>
2021-07-11 01:48:16 +0900

    feat: 다가오는 캔디가 null인 경우 구현

--- src/components/home/ComingCandyCard/index.tsx
+++ src/components/home/ComingCandyCard/index.tsx
@@ -1,5 +1,8 @@
 import styled from 'styled-components';
+import Image from 'next/image';
 import { DonutSmall } from '../../../../public/assets/candy';
+import { ComingCandyNull } from '../../../../public/assets/images';
+
 import OptionBar from './OptionBar';
 
 const Container = styled.div`
@@ -10,10 +13,12 @@ const Container = styled.div`
   margin-right: 16px;
   width: 188px;
   height: 285px;
-  font-family: Roboto;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans',
+    'Helvetica Neue', sans-serif;
+  filter: drop-shadow(0px 0px 14.3769px rgba(0, 0, 0, 0.09));
 `;
 
-const Thumbnail = styled.img`
+const Thumbnail = styled(Image)`
   z-index: 2;
   border-top-left-radius: 17px;
   border-top-right-radius: 17px;
@@ -29,7 +34,7 @@ const Metadata = styled.div`
   border-bottom-left-radius: 17px;
   border-bottom-right-radius: 17px;
   box-shadow: 5px 5px 20px #c4c4c471;
-  background-color: white;
+  background-color: var(--white);
   padding: 14px 0 20px 12px;
   width: 100%;
 `;
@@ -47,8 +52,9 @@ const Category = styled.h2`
 const Name = styled.h1`
   margin: 0;
   line-height: 21px;
-  color: #1e1e1e;
-  font-family: Roboto;
+  color: var(--black);
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans',
+    'Helvetica Neue', sans-serif;
   font-size: 18px;
   font-weight: 700;
   font-weight: bold;
@@ -71,13 +77,13 @@ export interface ComingCandyCardProps {
 export default function ComingCandyCard({ itemImage, category, name, plannedDate }: ComingCandyCardProps) {
   return (
     <Container>
-      <Thumbnail src={itemImage} />
+      <Thumbnail src={itemImage === '' ? ComingCandyNull : itemImage} width='188px' height='192px' />
       <OptionBar plannedDate={plannedDate} />
       <Metadata>
         <Category>{category}</Category>
         <Name>{name}</Name>
       </Metadata>
-      <Candy src={DonutSmall} />
+      {itemImage === '' ? null : <Candy src={DonutSmall} />}
     </Container>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
 import styled from 'styled-components';
 import RecommendCandyCard from '../components/home/RecommendCandyCard';
+import WaitingCardSlider from '../components/home/WaitingCardSlider';
 import ComingCandyCard from '../components/home/ComingCandyCard';
 
 const Container = styled.div`
   display: flex;
   margin: 0 auto;
   margin-top: 103px;
+  padding-bottom: 56px;
   max-width: 1440px;
 `;
 
@@ -13,23 +15,27 @@ const TitleContainer = styled.div`
   margin-right: 104px;
   line-height: 135%;
   letter-spacing: -0.022em;
-  font-family: NanumSquareRoundOTF;
-  font-size: 2.75rem;
+  font-family: 'NanumSquareRound', sans-serif;
   font-weight: 800;
+  font-size: 44px;
 
   & > p {
     box-sizing: border-box;
     display: flex;
     align-items: center;
-    justify-content: flex-start;
     margin-top: 21px;
     border: 1px solid var(--gray-3);
     border-radius: 18px;
-    background: var(--white);
     padding-left: 21px;
     width: 533px;
     height: 56px;
+    line-height: 130%;
+    letter-spacing: -0.022em;
+    color: var(--black);
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans',
+      'Helvetica Neue', sans-serif;
     font-size: 18px;
+    font-weight: normal;
   }
 `;
 
@@ -38,9 +44,8 @@ const ComingContainer = styled.div`
   width: 803px;
 `;
 
-const SubCandyContainer = styled.div`
+const FlexContainer = styled.div`
   display: flex;
-  flex-direction: row;
 `;
 
 const RecommendContainer = styled.div`
@@ -55,8 +60,8 @@ const CandyTitle = styled.div`
   margin-bottom: 4.79px;
   line-height: 32px;
   letter-spacing: -0.022em;
-  color: #000000;
-  font-family: NanumSquareRoundOTF;
+  color: var(--black);
+  font-family: 'NanumSquareRound', sans-serif;
   font-size: 1.75rem;
   font-weight: 800;
 `;
@@ -66,12 +71,9 @@ const CandyDesc = styled.div`
   line-height: 150%;
   letter-spacing: -0.022em;
   color: #808080;
-  font-family: Roboto;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans',
+    'Helvetica Neue', sans-serif;
   font-size: 18px;
-`;
-
-const ComingCandies = styled.div`
-  display: flex;
 `;
 
 const comingCandies = [
@@ -116,14 +118,17 @@ const recommendCandies = [
     content: '시원한 바람 맞으며 나들이가자!',
     image: 'https://dummyimage.com/74.68x74.68/000/fff',
   },
+  {
+    title: '한강으로 자전거 타러 가기',
+    content: '시원한 바람 맞으며 나들이가자!',
+    image: 'https://dummyimage.com/74.68x74.68/000/fff',
+  },
 ];
 
 const userInfo = {
   nickname: '다정',
   candyPhrase: '날 위한 달콤함을 잊지마세요',
-  month: 7,
   phrase: '한줄 문구입니다',
-  banner: '배너',
 };
 
 export default function Home() {
@@ -138,7 +143,7 @@ export default function Home() {
         <ComingContainer>
           <CandyTitle>다가오는 캔디</CandyTitle>
           <CandyDesc>행복을 안겨줄 캔디들이 곧 도착해요</CandyDesc>
-          <ComingCandies>
+          <FlexContainer>
             {comingCandies.length > 0 ? (
               comingCandies.map((candy, idx) => {
                 return (
@@ -152,15 +157,11 @@ export default function Home() {
                 );
               })
             ) : (
-              <ComingCandyCard
-                itemImage='https://dummyimage.com/188x192/000/fff'
-                category='내 손안의 달콤한 보상'
-                name='캔디를 추가해보세요'
-              />
+              <ComingCandyCard itemImage='' category='내 손안의 달콤한 보상' name='캔디를 추가해보세요' />
             )}
-          </ComingCandies>
+          </FlexContainer>
         </ComingContainer>
-        <SubCandyContainer>
+        <FlexContainer>
           <RecommendContainer>
             <CandyTitle>추천 캔디</CandyTitle>
             <CandyDesc>핸디캔디 추천으로 새로운 행복을 더해보세요</CandyDesc>
@@ -171,9 +172,9 @@ export default function Home() {
           <WaitingContainer>
             <CandyTitle>기다리는 캔디</CandyTitle>
             <CandyDesc> 담고만 있었던 캔디로 꺼내보세요 </CandyDesc>
-            {/* <WaitingCardSlider /> */}
+            <WaitingCardSlider />
           </WaitingContainer>
-        </SubCandyContainer>
+        </FlexContainer>
       </div>
     </Container>
   );
