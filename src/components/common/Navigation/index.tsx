6ed81b9bc2ed1101206f2c200acf24208980f845
찬순 <ckstns1231@gmail.com>
2021-07-11 17:01:28 +0900

    feat : framer-motion 설치 및 리뷰 반영.

--- src/components/common/Navigation/index.tsx
+++ src/components/common/Navigation/index.tsx
@@ -1,91 +1,49 @@
 import styled from 'styled-components';
-import React from 'react';
-const Container = styled.button`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  border: none;
+import React, { useState } from 'react';
+import { motion } from 'framer-motion';
+const buttonItems = ['전체 캔디', '분류별 캔디'];
+const Container = styled.span`
+  display: inline-block;
+  position: relative;
+  border: 8px solid #e9e9e9;
   border-radius: 100px;
   background: #e9e9e9;
-
-  width: 333px;
-  height: 61px;
 `;
-
-const TotalBtn = styled.button`
-  display: flex;
-  display: flex;
-  align-items: center;
-  align-items: center;
-  justify-content: center;
+const TabItem = styled.span`
+  box-sizing: border-box;
+  display: inline-block;
+  position: relative;
   border-radius: 25px;
-  background-color: white;
-  cursor: pointer;
+  padding: 15px 25px;
+  min-width: 155px;
+  text-align: center;
+  color: #1e1e1e;
+`;
+const TabIndicator = styled(motion.span)`
+  box-sizing: border-box;
+  position: absolute;
+  top: 0;
+  border-radius: 25px;
+  background: #fff;
   width: 155px;
   height: 48px;
-  text-align: center;
-  line-height: 26px;
-  font-family: Roboto;
-  font-size: 22px;
-  font-weight: 500;
-  font-style: normal;
 `;
-const CategoryBtn = styled.button`
-  display: flex;
-  display: flex;
-  align-items: center;
-  align-items: center;
-  justify-content: center;
-  border-radius: 25px;
-  background-color: #e9e9e9;
-  cursor: pointer;
-  width: 155px;
-  height: 48px;
-  text-align: center;
-  line-height: 26px;
-  font-family: Roboto;
-  font-size: 22px;
-  font-weight: 500;
-  font-style: normal;
-`;
-
 export default function Navigation() {
-  const totalRef = React.useRef<HTMLButtonElement>(null);
-  const categoryRef = React.useRef<HTMLButtonElement>(null);
-  const [curr, setCurr] = React.useState(0);
-  const toggleHandler = () => {
-    if (null !== totalRef.current) {
-      console.log(totalRef.current.style);
-      if (curr) {
-        totalRef.current.style.background = 'white';
-        totalRef.current.disabled = true;
-      } else {
-        totalRef.current.style.background = '#e9e9e9';
-        totalRef.current.disabled = false;
-        setCurr(1);
-      }
-    }
-    if (null !== categoryRef.current) {
-      console.log(categoryRef.current);
-      if (!curr) {
-        categoryRef.current.style.background = 'white';
-        categoryRef.current.disabled = true;
-      } else {
-        categoryRef.current.style.background = '#e9e9e9';
-        categoryRef.current.disabled = false;
-        setCurr(0);
-      }
-    }
-  };
-
+  const [activeIndex, setActiveIndex] = useState(0);
   return (
     <Container>
-      <TotalBtn onClick={toggleHandler} ref={totalRef}>
-        전체 캔디
-      </TotalBtn>
-      <CategoryBtn onClick={toggleHandler} ref={categoryRef}>
-        분류 캔디
-      </CategoryBtn>
+      <TabIndicator
+        animate={{
+          left: activeIndex === 0 ? 0 : 'auto',
+          right: activeIndex === 1 ? 0 : 'auto',
+        }}
+        transition={{ type: 'tween' }}
+      />
+      {buttonItems.map((value, index) => (
+        <TabItem key={value} onClick={() => setActiveIndex(index)}>
+          {value}
+        </TabItem>
+      ))}
     </Container>
   );
 }
