112114e40422b51ca56beeb57f21995ceed1a631
찬순 <ckstns1231@gmail.com>
2021-07-13 12:51:41 +0900

    feat : 담은 캔디 슬라이더 구현.

--- public/assets/icons/index.ts
+++ public/assets/icons/index.ts
@@ -10,9 +10,24 @@ export { default as Instagram } from './Instagram.svg';
 export { default as CategoryAdd } from './CategoryAdd.svg';
 export { default as ToggleButton } from './ToggleButton.svg';
 export { default as Check } from './Check.svg';
-export { default as BottomArrow } from './BottomArrow.svg';
-export { default as HandCream } from './HandCream.svg';
-export { default as LinkIcon } from './LinkIcon.svg';
+export { default as HandCream } from './handcream.svg';
+export { default as LinkIcon } from './link_icon.svg';
+export { default as BottomArrow } from './bottom_arrow.svg';
+<<<<<<< Updated upstream
 export { default as PlusIcon } from './edit_plus_icon.svg';
 export { default as BackArrow } from './back_arrow.svg';
 export { default as EditIcon } from './edit_icon.svg';
+=======
+export { default as UpIcon } from './upIcon.svg';
+export { default as AddIcon } from './Add.svg';
+export { default as WishedBackground } from './WishedBackground.svg';
+export { default as EachWishedBackground } from './EachWishedBackground.svg';
+export { default as PrevArrow } from './PrevArrow.svg';
+export { default as Go } from './Go.svg';
+export { default as LeftQuote } from './LeftQuote.svg';
+export { default as RightQuote } from './RightQuote.svg';
+export { default as EditBtn } from './Edit.svg';
+export { default as SliderLeftBtn } from './SliderLeftArrow.svg';
+
+export { default as SliderRightBtn } from './SliderRightArrow.svg';
+>>>>>>> Stashed changes
---
+++ src/components/common/WishedCandySlider/index.tsx
@@ -0,0 +1,127 @@
+import React from 'react';
+import Slider from 'react-slick';
+import styled from 'styled-components';
+import Image from 'next/image';
+import { useRouter } from 'next/router';
+import CandyCard from '../CandyCard';
+import { SliderRightBtn } from '../../../../public/assets/icons';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+const Container = styled.div`
+  /* width: 1450px; */
+  height: 420px;
+  .react__slick__slider__parent {
+    position: relative;
+  }
+
+  .react__slick__slider__parent .slick-prev,
+  .react__slick__slider__parent .slick-next {
+    position: absolute;
+    top: 45%;
+
+    /* width: 23px;
+    height: 47px; */
+  }
+
+  .react__slick__slider__parent .slick-prev {
+    color: black;
+  }
+  .react__slick__slider__parent .slick-next {
+    color: #c4c4c4;
+  }
+
+  .react__slick__slider__parent .slick-prev:before,
+  .slick-next:before {
+    color: #c4c4c4;
+  }
+  .react__slick__slider__parent .slick-prev:before {
+    content: url('/_next/image?url=%2F_next%2Fstatic%2Fimage%2Fpublic%2Fassets%2Ficons%2FSliderLeftArrow.6c26cf89495a445124b3c14ac0080f7c.svg&w=64&q=75');
+  }
+  .react__slick__slider__parent .slick-next:before {
+    content: url('/_next/image?url=%2F_next%2Fstatic%2Fimage%2Fpublic%2Fassets%2Ficons%2FSliderRightArrow.b96f375582e6a093583d32ee43089944.svg&w=64&q=75');
+  }
+  .slick-slider {
+    display: flex;
+    justify-content: center;
+    width: 1400px;
+  }
+  .slick-track {
+    display: flex;
+    justify-content: center;
+    width: 1400px !important;
+  }
+  .slick-slide {
+    display: flex;
+    justify-content: center;
+    margin-right: 30px;
+    width: 270px !important;
+    height: 450px;
+  }
+`;
+export default function WishedCandySlider() {
+  const settings = {
+    arrows: true,
+    infinite: true,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+
+    className: 'react__slick__slider__parent',
+  };
+  const date = new Date();
+  const router = new useRouter();
+  return (
+    <Container>
+      <Slider {...settings}>
+        <CandyCard
+          onClick={() => router.push({ pathname: '/wish/detail/[cid]', query: { cid: 0 } })}
+          itemImage='https://dummyimage.com/254x278/000/fff'
+          category='고생한 나 자신을 위한'
+          name='모베러웍스 티셔츠'
+          createdDate={15}
+          plannedDate={date}
+        />
+        <CandyCard
+          onClick={() => router.push({ pathname: '/wish/detail/[cid]', query: { cid: 0 } })}
+          itemImage='https://dummyimage.com/254x278/000/fff'
+          category='고생한 나 자신을 위한'
+          name='모베러웍스 티셔츠'
+          createdDate={15}
+          plannedDate={date}
+        />
+        <CandyCard
+          onClick={() => router.push({ pathname: '/wish/detail/[cid]', query: { cid: 0 } })}
+          itemImage='https://dummyimage.com/254x278/000/fff'
+          category='고생한 나 자신을 위한'
+          name='모베러웍스 티셔츠'
+          createdDate={15}
+          plannedDate={date}
+        />
+        <CandyCard
+          onClick={() => router.push({ pathname: '/wish/detail/[cid]', query: { cid: 0 } })}
+          itemImage='https://dummyimage.com/254x278/000/fff'
+          category='고생한 나 자신을 위한'
+          name='모베러웍스 티셔츠'
+          createdDate={15}
+          plannedDate={date}
+        />
+        <CandyCard
+          onClick={() => router.push({ pathname: '/wish/detail/[cid]', query: { cid: 0 } })}
+          itemImage='https://dummyimage.com/254x278/000/fff'
+          category='고생한 나 자신을 위한'
+          name='모베러웍스 티셔츠'
+          createdDate={15}
+          plannedDate={date}
+        />
+        <CandyCard
+          onClick={() => router.push({ pathname: '/wish/detail/[cid]', query: { cid: 0 } })}
+          itemImage='https://dummyimage.com/254x278/000/fff'
+          category='고생한 나 자신을 위한'
+          name='모베러웍스 티셔츠'
+          createdDate={15}
+          plannedDate={date}
+        />
+      </Slider>
+    </Container>
+  );
+}
