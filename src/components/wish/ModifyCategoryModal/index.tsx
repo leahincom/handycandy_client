37e1f7108c486b3e0b06f9b2e80a609b75c0b0e4
yoon <nicealice99@gmail.com>
2021-07-13 22:56:42 +0900

    feat: 담은캔디 카테고리 수정 모달 구현

---
+++ src/components/wish/ModifyCategoryModal/index.tsx
@@ -0,0 +1,185 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { Donut, Flower, Magnet, WaterDrop, Double } from '../../../../public/assets/candy';
+import Button from '../../common/Button';
+import CategoryDropdown from './CategoryDropdown';
+import ImageContainer from './ImageContainer';
+
+interface BackgroundProps {
+  isOpen: boolean;
+}
+
+const Background = styled.div<BackgroundProps>`
+  display: ${({ isOpen }) => (isOpen ? 'box' : 'none')};
+  position: fixed;
+  top: 0%;
+  left: 0%;
+  opacity: 0.4;
+  background-color: var(--black);
+  width: 100vw;
+  height: 100vh;
+`;
+
+const Container = styled.div<BackgroundProps>`
+  box-sizing: border-box;
+  display: ${({ isOpen }) => (isOpen ? 'flex' : 'none')};
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  flex-direction: column;
+  align-items: center;
+  transform: translate(-50%, -50%);
+  border-radius: 25px;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  background-color: var(--white);
+  padding-top: 47px;
+  padding-bottom: 42px;
+  width: 727px;
+  height: 711px;
+`;
+
+const Title = styled.div`
+  margin-bottom: 41px;
+  text-align: center;
+  line-height: 25px;
+  font-family: var(--nanum);
+  font-size: 22px;
+  font-weight: 800;
+`;
+
+const CategoryWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 29px;
+  margin-bottom: 33px;
+  line-height: 130%;
+  font-family: var(--roboto);
+  font-size: 18px;
+`;
+
+const CateogoryInfo = styled.div`
+  display: flex;
+  margin-top: 18px;
+
+  > span {
+    box-sizing: border-box;
+    display: flex;
+    align-items: center;
+    margin-left: 20px;
+    border: 1px solid var(--gray-3);
+    border-radius: 17px;
+    padding-left: 37px;
+    width: 365px;
+    height: 50px;
+    line-height: 28px;
+    color: var(--black);
+    font-family: var(--roboto);
+    font-size: 24px;
+    font-weight: bold;
+  }
+`;
+
+const DeleteWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 24px;
+  margin-bottom: 42px;
+  width: 486px;
+  line-height: 130%;
+  font-family: var(--roboto);
+  font-size: 18px;
+
+  p:nth-of-type(2) {
+    color: #5a5a5a;
+  }
+`;
+
+const DeleteButton = styled.div`
+  margin-top: 19px;
+  margin-bottom: 11px;
+  cursor: pointer;
+  line-height: 28px;
+  font-family: var(--roboto);
+  font-size: 24px;
+  font-weight: bold;
+`;
+
+const categories = [
+  {
+    image: Donut,
+    name: '행복한 나를 위한',
+  },
+  {
+    image: Magnet,
+    name: '바쁜 일상 후를 위한',
+  },
+  {
+    image: WaterDrop,
+    name: '휴식주기를 위한',
+  },
+  {
+    image: Flower,
+    name: '특별한 날을 위한',
+  },
+  {
+    image: Double,
+    name: '짜릿한 나를 위한',
+  },
+  {
+    image: Double,
+    name: '짜릿한 나를 위한',
+  },
+  {
+    image: Double,
+    name: '짜릿한 나를 위한',
+  },
+];
+
+export default function ModifyCategoryModal() {
+  const [isOpen, setIsOpen] = useState(true);
+  const [categoryId, setCategoryId] = useState<number>(0);
+  const [categoryName, setCategoryName] = useState<string>(categories[categoryId].name);
+
+  const handleCloseClick = () => {
+    setIsOpen(false);
+  };
+
+  return (
+    <>
+      <Background isOpen={isOpen} />
+      <Container isOpen={isOpen}>
+        <Title>카테고리 수정</Title>
+        <ImageContainer
+          candyImg='https://dummyimage.com/364x278/000/fff'
+          firstImg='https://dummyimage.com/184x255/000/fff'
+          secondImg='https://dummyimage.com/364x278/000/fff'
+          thirdImg='https://dummyimage.com/364x278/000/fff'
+        />
+        <CategoryWrapper>
+          <p>카테고리명</p>
+          <CateogoryInfo>
+            <CategoryDropdown
+              categories={categories}
+              categoryId={categoryId}
+              setCategoryId={setCategoryId}
+              setCategoryName={setCategoryName}
+            />
+            <span>{categoryName}</span>
+          </CateogoryInfo>
+        </CategoryWrapper>
+        <hr
+          style={{
+            border: '1px solid var(--gray-3)',
+            width: '486px',
+          }}
+        />
+        <DeleteWrapper>
+          <p>작업</p>
+          <DeleteButton>카테고리 삭제</DeleteButton>
+          <p>카테고리와 모든 캔디들은 영구적으로 삭제됩니다.</p>
+        </DeleteWrapper>
+        <Button text='완료' size='sm' buttonColor='peach' color='black' onClick={handleCloseClick} />
+      </Container>
+    </>
+  );
+}
