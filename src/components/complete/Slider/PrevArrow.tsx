6844056100523d197861f59c034a771bdaceea3e
juno <juno7803@naver.com>
2021-07-15 11:01:35 +0900

    chore: asset 네이밍 수정

--- src/components/complete/Slider/PrevArrow.tsx
+++ src/components/complete/Slider/PrevArrow.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 import styled from 'styled-components';
-import { PrevArrow as PrevArrowIcon } from '../../../../public/assets/icons';
+import { PreviousArrow as PrevArrowIcon } from '../../../../public/assets/icons';
 
 const ImageWrapper = styled.span``;
 
