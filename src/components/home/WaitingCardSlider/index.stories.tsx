01eccfb21a3eb37dff7d17428391523c353378d3
lebmouse <youngkwon@croquis.com>
2021-07-15 15:35:05 +0900

    chore:  스토리북 제거

--- src/components/home/WaitingCardSlider/index.stories.tsx
+++
@@ -1,8 +0,0 @@
-import { Meta, Story } from '@storybook/react';
-import WaitingCardSlider from '.';
-
-export default { title: 'components/home/WaitingCardSlider', component: WaitingCardSlider } as Meta;
-
-const Template: Story = (props) => <WaitingCardSlider {...props} />;
-
-export const Basic = Template.bind({});
